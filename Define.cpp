201312e9ce6ad34294be8b6db3897a62a21c00ac
qiwww <qiwangsophia@gmail.com>
2018-05-04 12:32:31 -0400

    Add files via upload

---
+++ Define.cpp
@@ -0,0 +1,16 @@
+// This is an example illustrating how to use #define
+#include <iostream>
+using namespace std;
+
+#define PI 3.14159;
+#define NEWLINE '\n';
+
+int main() {
+	double r = 5.0;
+	double circle;
+
+	circle = 2 * PI * r;			// circle = 2 * 3.14159 * r;
+	cout << circle << NEWLINE;		// cout << circle << '\n';
+
+	return 0;
+}
